4bdccd235e3fdd49437236ae5e24389254a1bc47
bienc <bienclashofclans@gmail.com>
2024-10-08 18:21:58 -0600

    primer add

---
+++ src/main/java/com/mycompany/gestioninventario/GestionInventario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestioninventario;
+
+/**
+ *
+ * @author Bienc
+ */
+public class GestionInventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
